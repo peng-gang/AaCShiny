b4683c912dfc19cc0b0c8165ac6ab114b6120a24
gpeng <michael.gang.peng@gmail.com>
2019-12-10 22:19:53 -0500

    getMeta

--- server.R
+++ server.R
@@ -14,19 +14,59 @@ shinyServer(function(input, output, session) {
       ana_sel <- input$analyte
       if(length(ana_sel) == 0){
         ## ERROR MESSAGE
+        updateSelectInput(
+          session, "analyte", selected = which(analytes_all=="C3")
+        )
+        meta <- meta_data[, which(analytes_all=="C3")]
+        return(list(meata=meta, name = "C3"))
       } else if(length(ana_sel) == 1){
         meta <- meta_data[, as.integer(ana_sel)]
-        names(meta) <- analytes_all[as.integer(ana_sel)]
-        return(meta)
+        return(list(meata=meta, name = analytes_all[as.integer(ana_sel)]))
       } else {
         meta <- rowSums(meta_data[, as.integer(ana_sel)])
-        names(meta) <- paste( analytes_all[as.integer(ana_sel)], collapse = " + ")
-        return(meta)
+        return(list(meata=meta, name = paste(analytes_all[sort(as.integer(ana_sel))], collapse = " + ")))
       }
-      
     } else {
       num_sel <- input$numerator
       den_sel <- input$denominator
+      meta_num <- NULL
+      meta_den <- NULL
+      name_num <- NULL
+      name_den <- NULL
+      
+      if(length(num_sel) == 0){
+        ## ERROR MESSAGE
+        updateSelectInput(
+          session, "numerator", selected = which(analytes_all=="C3")
+        )
+        meta_num <- meta_data[, which(analytes_all=="C3")]
+        name_num <- "C3"
+      } else if(length(num_sel) == 1){
+        meta_num <- meta_data[, as.integer(num_sel)]
+        name_num <- analytes_all[as.integer(num_sel)]
+      } else {
+        meta_num <- rowSums(meta_data[, as.integer(num_sel)])
+        name_num <- paste0("(", paste(analytes_all[sort(as.integer(num_sel))], collapse = " + "), ")")
+      }
+      
+      
+      if(length(den_sel) == 0){
+        ## ERROR MESSAGE
+        updateSelectInput(
+          session, "denominator", selected = which(analytes_all=="C2")
+        )
+        meta_den <- meta_data[, which(analytes_all=="C2")]
+        name_den <- "C2"
+      } else if(length(den_sel) == 1){
+        meta_den <- meta_data[, as.integer(den_sel)]
+        name_den <- analytes_all[as.integer(den_sel)]
+      } else {
+        meta_den <- rowSums(meta_data[, as.integer(den_sel)])
+        name_den <- paste0("(", paste(analytes_all[sort(as.integer(den_sel))], collapse = " + "), ")")
+      }
+      
+      meta <- meta_num / meta_den
+      return(list(meata=meta, name = paste0(name_num, "/", name_den)))
     }
   })
   
@@ -78,6 +118,6 @@ shinyServer(function(input, output, session) {
   
   
   output$boxplot <- renderPlot({
-    
+    hist(rnorm(100), main = getMeta()$name)
   })
 })
